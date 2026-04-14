71e24c0612137f60eb68b8a6d27a9dd7dca9b835
Richard Gonzalez <https://github.com/rich10anderson-ops/riccie-pallets-app.git>
2026-04-14 14:18:32 -0500

    fix: agrega configuración del servidor y vista previa en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+  },
+  preview: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+  },
   build: {
     outDir: 'dist',
     rollupOptions: {
